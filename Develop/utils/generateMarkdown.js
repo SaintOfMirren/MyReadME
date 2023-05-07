0fe9d653d213544e35d1808f6e870d5fafe7c15c
SaintOfMirren <drew.miller2015@gmail.com>
2023-05-07 11:14:03 -0700

    tried to add licensing link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -20,11 +20,39 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  const links = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+  };
+
+  const linkUrl = links[license];
+
+  if (!linkUrl) {
+    throw new Error(`Unsupported license: ${license}`);
+  }
+
+  return `(${linkUrl})`;
+ }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `
+## License
+
+This project is licensed under the ${license} License. Click [here]${renderLicenseLink(license)} for more information.
+
+${renderLicenseBadge(license)}
+`;
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
